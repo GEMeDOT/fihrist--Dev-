a84ddc0cc037570abd3ff54dc013e5610ad43cc0
Courtney Roes <CRoes@GEMeDOT.com>
2013-08-23 14:30:35 +0200

    MP3s now pulled from S3 bucket

--- app/store/Verses.js
+++ app/store/Verses.js
@@ -26,91 +26,91 @@ Ext.define('Fihrist.store.Verses', {
         data: [
             {
                 UID: 'V1',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Rom8_18-22.mp3',
+                MP3URL: 'Rom8_18-22.mp3',
                 LocalURL: '',
                 text: 'Rom.8:18-22'
             },
             {
                 UID: 'V2',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Luk13_1-5.mp3',
+                MP3URL: 'Luk13_1-5.mp3',
                 LocalURL: '',
                 text: 'Luk.13:1-5'
             },
             {
                 UID: 'V3',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Yuh9_1-3.mp3',
+                MP3URL: 'Yuh9_1-3.mp3',
                 LocalURL: '',
                 text: 'Yuh.9:1-3'
             },
             {
                 UID: 'V4',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Rom5_3-5.mp3',
+                MP3URL: 'Rom5_3-5.mp3',
                 LocalURL: '',
                 text: 'Rom.5:3-5'
             },
             {
                 UID: 'V5',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Yak1_2-4.mp3',
+                MP3URL: 'Yak1_2-4.mp3',
                 LocalURL: '',
                 text: 'Yak.1:2-4'
             },
             {
                 UID: 'V6',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/2Ko1_3-11.mp3',
+                MP3URL: '2Ko1_3-11.mp3',
                 LocalURL: '',
                 text: '2Ko.1:3-11'
             },
             {
                 UID: 'V7',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Rom8_28.mp3',
+                MP3URL: 'Rom8_28.mp3',
                 LocalURL: '',
                 text: 'Rom.8:28'
             },
             {
                 UID: 'V8',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/2Ko4_16-18.mp3',
+                MP3URL: '2Ko4_16-18.mp3',
                 LocalURL: '',
                 text: '2Ko.4:16-18'
             },
             {
                 UID: 'V9',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/2Ko1_3-11.mp3',
+                MP3URL: '2Ko1_3-11.mp3',
                 LocalURL: '',
                 text: '2Ko.1:3-11'
             },
             {
                 UID: 'V10',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Vah21_4.mp3',
+                MP3URL: 'Vah21_4.mp3',
                 LocalURL: '',
                 text: 'Vah.21:4'
             },
             {
                 UID: 'V11',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Mat5_10-13.mp3',
+                MP3URL: 'Mat5_10-13.mp3',
                 LocalURL: '',
                 text: 'Mat.5:10-13'
             },
             {
                 UID: 'V12',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Mat24_9.mp3',
+                MP3URL: 'Mat24_9.mp3',
                 LocalURL: '',
                 text: 'Mat.24:9'
             },
             {
                 UID: 'V13',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Yuh15_18-25.mp3',
+                MP3URL: 'Yuh15_18-25.mp3',
                 LocalURL: '',
                 text: 'Yuh.15:18-25'
             },
             {
                 UID: 'V14',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Vah6_9-11.mp3',
+                MP3URL: 'Vah6_9-11.mp3',
                 LocalURL: '',
                 text: 'Vah.6:9-11'
             },
             {
                 UID: 'V15',
-                MP3URL: 'http://www.kutsalkitap.org/fihrist/Fihrist/sesdosyalar/Ibr5_7-9.mp3',
+                MP3URL: 'Ibr5_7-9.mp3',
                 LocalURL: '',
                 text: 'İbr.5:7-9'
             }
