094666289d1990e9eb98c2f65203eb377ea5a472
Courtney Roes <CRoes@GEMeDOT.com>
2013-10-06 14:46:56 +0200

    Added MP3 slider control

--- app/view/DetailsView.js
+++ app/view/DetailsView.js
@@ -36,8 +36,17 @@ Ext.define('Fihrist.view.DetailsView', {
                 iconAlign: 'center'
             },
             {
+                xtype: 'sliderfield',
+                centered: false,
+                docked: 'top',
+                itemId: 'mp3slider',
+                margin: '0 auto 5px',
+                maxWidth: '80%',
+                labelAlign: 'right',
+                name: 'MP3Slider'
+            },
+            {
                 xtype: 'component',
-                border: 5,
                 centered: true,
                 itemId: 'versetext',
                 ui: 'light',
@@ -54,11 +63,46 @@ Ext.define('Fihrist.view.DetailsView', {
                 fn: 'onControlsTap',
                 event: 'tap',
                 delegate: '#controls'
+            },
+            {
+                fn: 'onMp3sliderDrag',
+                event: 'drag',
+                delegate: '#mp3slider'
+            },
+            {
+                fn: 'onMp3sliderDragEnd',
+                event: 'dragend',
+                delegate: '#mp3slider'
             }
         ]
     },
 
     onMp3playerTimeupdate: function(media, time, eOpts) {
+        var duration = media.getDuration();
+        var mp3slider = media.getParent().down('sliderfield');
+        var position = (time / duration) * 100;
+        mp3slider.setValue(position);
+
+        var tick = Math.floor(duration);
+        var mins = Math.floor(tick/60);
+        var secs = tick % 60;
+        var tock = (mins < 10 ? "0" : "" ) + mins + ":" + (secs < 10 ? "0" : "" ) + secs;
+
+        duration = tock;
+
+        tick = Math.floor(time);
+        mins = Math.floor(tick/60);
+        secs = tick % 60;
+        tock = (mins < 10 ? "0" : "" ) + mins + ":" + (secs < 10 ? "0" : "" ) + secs;
+        position = tock;
+
+        mp3slider.setLabel( position + '<br /><hr />' + duration);
+
+        var button = media.getParent().down('button');
+        if ( position == duration ) {
+            media.pause();
+            button.setHtml('<img src="resources/btns/play.png" />');
+        }
 
     },
 
@@ -70,7 +114,59 @@ Ext.define('Fihrist.view.DetailsView', {
         } else {
             audio.play();
             button.setHtml('<img src="resources/btns/pause.png" />');
+            var audioDurationnum = audio.getDuration();
+
+            var mp3slider = this.getParent().down('sliderfield');
+
+            var tick = Math.floor(audioDurationnum);
+            var mins = Math.floor(tick/60);
+            var secs = tick % 60;
+            var tock = (mins < 10 ? "0" : "" ) + mins + ":" + (secs < 10 ? "0" : "" ) + secs;
+
+            mp3slider.setLabel(tock);
         }
+    },
+
+    onMp3sliderDrag: function(sliderfield, sl, thumb, e, eOpts) {
+        var audio = this.getParent().down('audio');
+        audio.pause();
+
+        var button = this.getComponent('controls');
+        button.setHtml('<img src="resources/btns/play.png" />');
+    },
+
+    onMp3sliderDragEnd: function(sliderfield, sl, thumb, value, e, eOpts) {
+        var audio = this.getComponent('mp3player');
+
+        var duration = audio.getDuration();
+
+        var position = value;
+        sliderfield.setValue(position);
+
+        position = ((value / 100) * duration);
+
+        audio.setCurrentTime(position);
+
+        var tick = Math.floor(duration);
+        var mins = Math.floor(tick/60);
+        var secs = tick % 60;
+        var tock = (mins < 10 ? "0" : "" ) + mins + ":" + (secs < 10 ? "0" : "" ) + secs;
+
+        duration = tock;
+
+
+        tick = Math.floor(position);
+        mins = Math.floor(tick/60);
+        secs = tick % 60;
+        tock = (mins < 10 ? "0" : "" ) + mins + ":" + (secs < 10 ? "0" : "" ) + secs;
+        position = tock;
+
+        sliderfield.setLabel( position + '<br /><hr />' + duration);
+
+        audio.play();
+
+        var button = this.getComponent('controls');
+        button.setHtml('<img src="resources/btns/pause.png" />');
     }
 
 });
